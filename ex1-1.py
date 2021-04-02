3e4caf5e8601743998aa46f2d3c3847f9bf12d3d
NadavGit <nadavk90@gmail.com>
2021-04-02 22:06:17 +0300

    clearer variable explanation

--- ex1-1.py
+++ ex1-1.py
@@ -13,21 +13,21 @@ times = [ 1, 60, 60*60, 24*(60*60), 30*(24*(60*60)), 365*(24*(60*60)), 100*(365*
 ### ----- FUNCTIONS ---- ###
 
 def first_line():
-    print('{:10s} {:25s} {:25s} {:25s} {:25s} {:25s} {:25s} {:25s}'.format(" ","1 second","1 minute","1 hour","1 day","1 month"," 1 year","1 century")) ## time periods
+    print('{:10s} {:25s} {:25s} {:25s} {:25s} {:25s} {:25s} {:25s}'.format(" ","1 second","1 minute","1 hour","1 day","1 month","1 year","1 century")) ## time periods
     
 def lgn():  
-    print("\n"+'{:10s}'.format("log(n)"), end =" ")
+    print("\n" + '{:10s}'.format("log(n)"), end = " ")
     for time in times:
         x = format((time/micros), ".3e")
-        print('{:25s}'.format("2 ** ( "+str(x) + " )"), end = " ")
+        print('{:25s}'.format("2 ** ( " + str(x) + " )"), end = " ")
 
 def sqrt_n():
-    print("\n\n"+'{:10s}'.format("sqrt(n)"), end =" ")
+    print("\n\n" + '{:10s}'.format("sqrt(n)"), end = " ")
     for time in times:
-        x = format(math.floor((time/micros)**2), ".3e")
-        print('{:25s}'.format(str(x)), end =" ")
+        x = format(math.floor( (time/micros)**2 ), ".3e")
+        print('{:25s}'.format(str(x)), end = " ")
 def n():
-    print("\n\n"+'{:10s}'.format("n"), end = " ")
+    print("\n\n" + '{:10s}'.format("n"), end = " ")
     for time in times:
         x = format(math.floor((time/micros)), ".2e")
         print('{:25s}'.format(str(x)), end = " ")
@@ -35,16 +35,11 @@ def n():
 def nlogn():
     # using NEWTON - RAPHSON METHOD
     print("\n\n"+'{:10s}'.format("nlog(n)"), end = " ")
-    sensitivity = 0
     func = lambda x: x * math.log(x,2) - time/micros     #the function whose root we wish to find
     deriv = lambda x: math.log(x,2) + 1/(math.log(2)) # derivative of the above function
-    for time in times:       
-        x = 10 ** 2 #initial guess 
-        y = func(x)
-        while (abs(y) > sensitivity):
-            x = x - func(x)/deriv(x)
-            y = func(x)
-        x = format(math.floor(x), ".2e")
+    for time in times:
+        x = newton_raphson(func, deriv, 0 , 10 ** 2)
+        x = format(math.floor(x), ".2e") 
         print('{:25s}'.format(str(x)), end = " ")
 
 def n_squared():
@@ -54,18 +49,12 @@ def n_squared():
         print('{:25s}'.format(str(x)),end = " ")
         
 def n_cubed():
-    # using NEWTON - RAPHSON METHOD (because naive use of pow function causes floating-point representation issues)
+    # using NEWTON - RAPHSON METHOD (since naive use of pow function causes floating-point representation issues)
     print("\n\n"+'{:10s}'.format("n ** 3"), end = " ")
-    sensitivity = 1
     func = lambda x: x ** 3 - time/micros   #the function whose root we wish to find
     deriv = lambda x: 3 *(x ** 2)           # derivative of the above function
-    for time in times:
-        x = 10 ** 5  #initial guess 
-        y = func(x)
-        while (abs(y) > sensitivity):
-            x = x - func(x)/deriv(x)
-            y = func(x)
-        x = math.floor(x)
+    for time in times: 
+        x = newton_raphson(func, deriv, 1 , 10 ** 5)
         print('{:25s}'.format(str(x)), end = " ")
 
 def two_power_n():
@@ -84,6 +73,15 @@ def n_fact():
             x *= count
         count -= 1 #reducing count by 1 to get last value smaller than a bound
         print('{:25s}'.format(str(count)), end = " ")
+
+def newton_raphson(func, deriv, sensitivity , x0): #x0 - initial guess
+    x = x0
+    y = func(x)
+    while (abs(y) > sensitivity):
+            x = x - func(x)/deriv(x)
+            y = func(x)
+    return math.floor(x) # achieving natural number by flooring
+    
 
     
 first_line()        
